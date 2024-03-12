5fbaa99d3570f0b98c843a04eab2d4831c3f2f91
Nikul <Nikul>
2024-03-12 15:32:17 +0530

    new changes

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -18,7 +18,7 @@ class OrderController extends Controller
 
         $file = $request->file('csv_file');
 
-        try {
+        try { 
             // Read CSV file
             $reader = Reader::createFromPath($file->getPathname(), 'r');
             $reader->setHeaderOffset(0);
